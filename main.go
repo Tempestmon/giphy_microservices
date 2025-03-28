5a1b55f0537f1bbbd10c66855316c6a6879fb211
Tempestmon <misha8936@yandex.ru>
2025-03-28 16:04:26 +0500

    feat: simple gin implementation

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	apiservice "giphy_microservices/api_service"
+)
 
 func main() {
-	fmt.Println("Kek")
+	// app := app.InitApp()
+	// if app.HttpServer == nil {
+	// 	log.Fatal("Server is nil - Wire injection failed")
+	// }
+	// log.Fatal(app.HttpServer.Start())
+	apiservice.NewServer("127.0.0.1:8080").Start()
 }
