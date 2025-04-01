a2f2734f160734ce3d83d4c6a178ad81c9633673
Tempestmon <misha8936@yandex.ru>
2025-04-01 22:48:51 +0500

    fix: backend proper import

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,3 @@
-module backend
+module github.com/tempestmon/giphy_microservices/backend
 
 go 1.24.0
--- backend/server/server.go
+++ backend/server/server.go
@@ -1,5 +1,3 @@
 package server
 
-// import "github.com/giphy_microservices/server/gen"
-
 type ServerAPI struct{}
